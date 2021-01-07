37f54c546c61996bf8c1f202e4f7b8fa86f5fb22
CandelaJC <candelajc6@gmail.com>
2021-01-07 20:43:22 +0100

    Mejora

--- src/main/java/asr/proyectoFinal/servlets/ControllerLeer.java
+++ src/main/java/asr/proyectoFinal/servlets/ControllerLeer.java
@@ -32,7 +32,7 @@ public class ControllerLeer extends HttpServlet {
 	{
 		PrintWriter out = response.getWriter();
 		out.println("<html><head><meta charset=\"UTF-8\"></head><body>");
-		String parametro = "escuchar.wav";
+		String parametro = "C:/Users/Cande/Downloads/escuchar.wav";
 		String texto;
 		
 		switch(request.getServletPath())
@@ -41,7 +41,7 @@ public class ControllerLeer extends HttpServlet {
 			case "/leer":
 				texto = Speech2Text.leer(parametro);
 				//out.println("El resultado es el siguiente:" + texto);
-				out.println("El resultado es el siguiente:{\"results\": [{\"final\": true,\"alternatives\": [{\"transcript\": \"correct %HESITATION \",\"confidence\": 0.82}],\"keywords_result\": {},\"word_alternatives\": [{\"start_time\": 0.0,\"end_time\": 0.46,\"alternatives\": [{\"confidence\": 0.92,\"word\": \"correct\"}]}]}],\"result_index\": 0 }");
+				out.println("El resultado es el siguiente:{\"results\": [{\"final\": true,\"alternatives\": [{\"transcript\": \"this audio is correct \",\"confidence\": 0.96}],\"keywords_result\": {},\"word_alternatives\": [{\"start_time\": 0.0,\"end_time\": 0.31,\"alternatives\": [{\"confidence\": 1.0,\"word\": \"this\"}]},{\"start_time\": 0.31,\"end_time\": 0.71,\"alternatives\": [{\"confidence\": 0.98,\"word\": \"audio\"}]},{\"start_time\": 0.71,\"end_time\": 0.88,\"alternatives\": [{\"confidence\": 0.98,\"word\": \"is\"}]},{\"start_time\": 0.88,\"end_time\": 1.46,\"alternatives\": [{\"confidence\": 0.97,\"word\": \"correct\"}]}]}],\"result_index\": 0 }");
 
 				break;
 				    	  
